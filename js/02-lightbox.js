af6cc81f21728b13a68513483894e39a6a4e8d04
Nannielf <nannielf.no@gmail.com>
2023-10-11 23:50:05 +0300

    2-1

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,15 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const gallery = document.querySelector('.gallery');
+function markup(arr){
+    return arr.map(({preview, original, description}) => `
+    <li class="gallery__item">
+   <a class="gallery__link" href="${original}" onclick="event.preventDefault()">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>`).join('')
+};
+gallery.insertAdjacentHTML('afterbegin', markup(galleryItems));
+new SimpleLightbox('.gallery__item a', { /* options */ });
 
 console.log(galleryItems);
